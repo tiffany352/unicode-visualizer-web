81f3fba564112d5f6aab822ca9b8263367642dd2
Tiffany Bennett <tiffnixen@gmail.com>
2018-11-18 00:16:36 -0800

    Better presentation of properties in info page

--- src/components/CodepointPage.js
+++ src/components/CodepointPage.js
@@ -12,6 +12,46 @@ function decimalToHex (d, padding) {
   return hex
 }
 
+const generalCategory = {
+  Lu: 'Letter, uppercase',
+  Ll: 'Letter, lowercase',
+  Lt: 'Letter, titlecase',
+  Lm: 'Letter, modifier',
+  Lo: 'Letter, other',
+  Mn: 'Mark, nonspacing',
+  Mc: 'Mark, spacing combining',
+  Me: 'Mark, enclosing',
+  Nd: 'Number, decimal digit',
+  Nl: 'Number, letter',
+  No: 'Number, other',
+  Pc: 'Punctuation, connector',
+  Pd: 'Punctuation, dash',
+  Ps: 'Punctuation, open',
+  Pe: 'Punctuation, close',
+  Pi: 'Punctuation, initial quote',
+  Pf: 'Punctuation, final quote',
+  Po: 'Punctuation, other',
+  Sm: 'Symbol, math',
+  Sc: 'Symbol, currency',
+  Sk: 'Symbol, modifier',
+  So: 'Symbol, other',
+  Zs: 'Separtaor, space',
+  Zl: 'Separtaor, line',
+  Zp: 'Separator, paragraph',
+  Cc: 'Other, control',
+  Cf: 'Other, format',
+  Cs: 'Other, surrogate',
+  Co: 'Other, private use',
+  Cn: 'Other, not assigned'
+}
+
+const propsTable = {
+  blk: 'Block',
+  na: 'Name',
+  age: (value) => ['Appeared', 'Unicode ' + value],
+  gc: (value) => ['General Category', `${generalCategory[value]} (${value})`]
+}
+
 class CodepointPage extends React.Component {
   constructor (props) {
     super(props)
@@ -28,22 +68,50 @@ class CodepointPage extends React.Component {
         </div>
       )
     }
+
+    const convertToDisplay = (prop, value) => {
+      let displayName = propsTable[prop] || prop
+      let displayValue = value
+      const entry = propsTable[prop]
+      if (entry instanceof Function) {
+        [displayName, displayValue] = entry(value)
+      }
+      return [displayName, displayValue]
+    }
+
     const data = []
-    if (this.state.data) {
-      for (const [name, value] of Object.entries(this.state.data)) {
-        data.push(
-          <li key={name}>{name} = {value}</li>
-        )
+    for (const [prop, entry] of Object.entries(propsTable)) {
+      data.push(convertToDisplay(prop, props[prop]))
+    }
+    const yesValues = []
+    const noValues = []
+    for (const [prop, value] of Object.entries(props)) {
+      if (value == 'Y') {
+        yesValues.push(convertToDisplay(prop, value)[0])
+      }
+      else if (value == 'N') {
+        noValues.push(convertToDisplay(prop, value)[0])
       }
     }
+    data.sort((a, b) => a[0] < b[0])
+    data.push(['Has Properties', yesValues.join(', ')])
+    data.push(['Missing Properties', noValues.join(', ')])
+
+    const elements = data.map(([name, value]) => (
+      <React.Fragment key={name}>
+        <dt>{name}</dt>
+        <dd>{value}</dd>
+      </React.Fragment>
+    ))
+
     return (
       <div className="CodepointPage-container">
         <h1>
           U+{decimalToHex(this.props.codepoint, 4)} {props.na}
         </h1>
-        <ul>
-          {data}
-        </ul>
+        <dl>
+          {elements}
+        </dl>
       </div>
     )  
   }
