7ccc72264271993c55c0012fd68b887530d5e907
Tiffany Bennett <tiffany@tiffnix.com>
2022-12-17 22:08:47 -0800

    Fix warning

--- src/lib/server/Data.ts
+++ src/lib/server/Data.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import fs from "fs";
+import * as fs from "fs";
 import { parse, parseTabs } from "./SemicolonValues";
 import * as t from "./Types";
 
